b83a6f3a62090f2bff6f3a14d536d40135a0ba24
Mukesh Pandey <panditmukki5@gmail.com>
2024-07-10 01:54:36 +0530

    model change

--- src/models/master/masterLiquorModel.js
+++ src/models/master/masterLiquorModel.js
@@ -2,15 +2,7 @@ import mongoose, { Schema } from "mongoose";
 
 const LiquorSchema = new Schema({
   brandName: { type: String, required: true },
-  taxes: {
-    vat: { type: Number, required: true }, //Commercial tax
-    cess: { type: Number, required: true }, //Cess tax
-    wep: { type: Number, required: true }, //Women Empowerment Cow Protection Sports Activity Cess
-    hologram: { type: Number, required: true }, //Hologram & track and trace fee
-    flProfit: { type: Number, required: true }, //Profit of FL-2 Licens e Holder (Per Case)
-    pratifal: { type: Number, required: true }, //pratifal Fee
-    tcs: { type: Number, required: true, default: 1 },
-  },
+
   company: {
     type: mongoose.Types.ObjectId,
     ref: "MasterCompany",
@@ -20,6 +12,9 @@ const LiquorSchema = new Schema({
     {
       size: { type: Number, required: true },
       price: { type: Number, required: true },
+      wep: { type: Number, required: true }, //Women Empowerment Cow Protection Sports Activity Cess
+      hologram: { type: Number, required: true }, //Hologram & track and trace fee
+      pratifal: { type: Number, required: true }, //pratifal Fee
     },
   ],
 });
